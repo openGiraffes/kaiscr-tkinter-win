2a34421a09a2642a8c84fee4848e8a887e6f9042
Farooq Karimi Zadeh <hapu@riseup.net>
2020-10-21 10:51:34 +0330

    Some enhancements

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # KaiScr
-### Small Python scripts to take screenshot from a KaiOS/FFOS device and to mirror the screen of your phone.
+## Small Python scripts to take screenshot from a KaiOS/FFOS device and to mirror the screen of your phone.
 
+A video on how to use it from myself:
+http://far.chickenkiller.com/files/using-kailive-and-kaiscr.mp4
+
+### kaiscr.py
+Just run the script with `-h`:
+```
+python3 kaiscr.py -h
+```
+
+### kailive.py
+Just run the script and make sure you have forwarded the debug socket
+to port 6000(TCP):
+```
+python3 kailive.py
+```
+Press `q` to quit.
--- kailive.py
+++ kailive.py
@@ -1,34 +1,54 @@
 import threading
 import time
 import kaiscr
-from gi.repository import Gtk, GdkPixbuf, GLib
+import sys
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GdkPixbuf, GLib, Gdk
 
-takescreenshot = kaiscr.TakeScreenshot().screenshot
-window = Gtk.Window()
-window.connect("destroy", Gtk.main_quit)
+takescreenshot = kaiscr.TakeScreenshot()
+screenshot = takescreenshot.screenshot
+bye = takescreenshot.close
 img = None
+stop = False
+
+
+def quit(*args):
+    global stop
+    stop = True
+    bye()
+    Gtk.main_quit()
+
+def on_keypress(widget, event):
+    if event.keyval == 113: # q key
+        quit()
 
 def update_pic():
     global img
     global takescreenshot
+    try:
+        while not stop:
+            loader = GdkPixbuf.PixbufLoader()
+            png = screenshot()
+            print(png)
+            loader.write(png)
+            pb = loader.get_pixbuf()
+            if not img:
+                img = Gtk.Image.new_from_pixbuf(pb)
+            else:
+                img.set_from_pixbuf(pb)
+            loader.close()
+    except Exception as e:
+        print(e)
 
-    while 1:
-        loader = GdkPixbuf.PixbufLoader()
-        loader.write(takescreenshot())
-        pb = loader.get_pixbuf()
-        if not img:
-            img = Gtk.Image.new_from_pixbuf(pb)
-        else:
-            img.set_from_pixbuf(pb)
-        loader.close()
 
-
-t = threading.Thread(target=update_pic)
-t.start()
+threading.Thread(target=update_pic).start()
+window = Gtk.Window()
+window.connect("destroy", quit)
+window.connect("key-press-event", on_keypress)
+window.set_type_hint(Gdk.WindowTypeHint.UTILITY)
 while not img:
     time.sleep(0.1)
 window.add(img)
 window.show_all()
 Gtk.main()
-
-
